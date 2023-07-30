b643dfa3e7a7649dc2357188f8d3b6a4edde8cba
Zatanna Lyte <>
2023-07-30 22:01:26 +0300

    config/wip -> db works, minor changes in .env (db-name from 'image-box' to 'imagebox'

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.16.0
 )
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/kelseyhightower/envconfig"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"github.com/zenorachi/image-box/pkg/database/postgres"
 )
