2cd2cd3d2892f196794e84302d486de2973c3c2d
Zatanna Lyte <>
2023-07-30 21:33:16 +0300

    config/wip -> add configs (.yml, .env), started working on db

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/viper"
+	"github.com/zenorachi/image-box/pkg/database/postgres"
+)
+
+type Config struct {
+	Server struct {
+		Port int
+	}
+
+	DB postgres.DBConfig
+}
+
+func New(directory, filename string) (*Config, error) {
+	cfg := new(Config)
+
+	viper.AddConfigPath(directory)
+	viper.SetConfigName(filename)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := viper.Unmarshal(cfg); err != nil {
+		return nil, err
+	}
+
+	if err := envconfig.Process("db", &cfg.DB); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Name     string
+	SSLMode  string
+	Password string
+}
+
+func NewDB(cfg DBConfig) (*sql.DB, error) {
+	source := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s", cfg.Host, cfg.Port, cfg.Username, cfg.Name, cfg.SSLMode, cfg.Password)
+	db, err := sql.Open("postgres", source)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
